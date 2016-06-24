0c592b12a8f3e7c8fc30888b41d710171902203c
pawel-krysiak <pawel.krysiak@rupeal.com>
2016-06-24 16:26:11 +0100

    changed package url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='librato_chart_sender',
       version='0.1',
       description='Rupeals library to send Librato charts via email',
-      url='https://github.com/weareswat/librato-chart-sender',
+      url='https://github.com/weareswat/librato_chart_sender_package',
       author='Goncalo Correia / Pawel Krysiak / SWAT - Rupeal',
       author_email='swat@weareswat.com',
       license='MIT',
