9ae8d38c5f5d2e234e9c3f980e1e676a04b2ddcf
DJGuu <goncalo.correia@rupeal.com>
2016-06-28 12:30:49 +0100

    changed one line with updated information

--- chart_sender/main.py
+++ chart_sender/main.py
@@ -47,5 +47,5 @@ class LibratoChartSender():
 			print "E-mail sent succesfully"
 		
 #
-# chart_sender = LibratoChartSender( [3419, 3420, 3421], ['pawel.krysiak@rupeal.com'], {'librato_api_key' : 'b4bf0341c8cdd3b429826a18d1a07582895fa12c7fb97eb8f2c6bdb015004b86', 'mailgun_api_key' : 'key-a05af654983f6c57ec99904a3b84c7b3' })
+# chart_sender = LibratoChartSender('systems@rupeal.com, 604800, [3419, 3420, 3421], ['pawel.krysiak@rupeal.com'], {'librato_api_key' : 'b4bf0341c8cdd3b429826a18d1a07582895fa12c7fb97eb8f2c6bdb015004b86', 'mailgun_api_key' : 'key-a05af654983f6c57ec99904a3b84c7b3' })
 # chart_sender.run()
